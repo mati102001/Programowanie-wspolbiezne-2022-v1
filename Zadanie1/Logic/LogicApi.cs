36665f3cf52ab5edb6d269b0d5926e76990364b8
Krzysztof Izydorczyk <236545@edu.p.lodz.pl>
2022-04-21 17:43:06 +0200

    Small changes

---
+++ Zadanie1/Logic/LogicApi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public abstract class LogicAbstractApi {
+        public abstract int screenWidth { get; }
+        public abstract int screenHeight { get; }
+        public abstract void CreateBall(int number);
+        public abstract List<Ball> GetBalls();
+    }
+
+    public class LogicApi : LogicAbstractApi
+    {
+        public override int screenWidth { get; }
+        public override int screenHeight { get; }
+        private List<Ball> _balls = new List<Ball>();
+
+        public List<Ball> Balls { get { return _balls; } }
+
+        public override void CreateBall(int number)
+        {
+            Random rnd = new Random();
+            
+            for(int i = 0; i < number; i++)
+            {
+                int randomR = rnd.Next(1, 20);
+                float randomX = rnd.Next(randomR, (int) (screenWidth - randomR));
+                float randomY = rnd.Next(randomR, (int) (screenWidth - randomR));
+
+                _balls.Add(new Ball(randomX, randomY, randomR));
+            }
+        }
+
+        public override List<Ball> GetBalls()
+        {
+            return _balls;
+        }
+    }
+
+
+
+}
